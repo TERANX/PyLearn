f2a109fe5ee514c35fe0750dd00c9d54da931e90
TERANX <Teranoid777@yandex.ru>
2023-08-11 21:21:48 +0300

    While Loop

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+import turtle as t
+
+RND = 360  # 360 градусов в замкнутой фигуре
+sides = 4  # число сторон
+figs = 8  # число фигур
+f_angle = RND / figs  # угол для наклона фигур
+angle = RND / sides  #
+dist = 120  # дистанция
+count = 0  # счетчик
+fig_count = 0
+
+while fig_count < figs:
+    count = 0
+    while count < sides:
+        t.forward(dist)
+        t.right(angle)
+        count += 1
+    fig_count += 1
+    t.right(f_angle)
+
+t.mainloop()
+
 a, b = 10, 5  # объявил переменные
 
 print(a, '+', b, '=', a + b)
@@ -11,7 +33,7 @@ print(a, 'в степени', b, '=', a ** b)
 a += 1  # инкремент
 a -= 1  # декримент
 
-#Ввод данных и сильная типизация
+# Ввод данных и сильная типизация
 coffee = input('Сколько стоил кофе: ')
 donut = input('Сколько стоил пончик: ')
 print(type(coffee))
